7191ffa8108a4a803d174f9d9e68524d33d6fd09
Erik Shafer <erik.shafer@gmail.com>
2021-12-19 19:45:26 -0600

    Initial draft of aggregate related classes.

---
+++ src/EventSorcery/Aggregates/Aggregate.cs
@@ -0,0 +1,22 @@
+﻿namespace EventSorcery.Aggregates;
+
+public abstract class Aggregate
+{
+    private readonly List<object> _changes = new();
+    
+    public IReadOnlyCollection<object> Changes => _changes.AsReadOnly();
+
+    public void ClearChanges() => _changes.Clear();
+
+    public abstract void Load(IEnumerable<object> events);
+
+    public abstract void Fold(object @event);
+
+    public void AddChange(object @event) => _changes.Add(@event);
+}
+
+public abstract class Aggregate<T> : Aggregate
+    where T : AggregateState<T>, new()
+{
+    
+}
---
+++ src/EventSorcery/Aggregates/AggregateId.cs
@@ -0,0 +1,20 @@
+﻿namespace EventSorcery.Aggregates;
+
+public abstract record AggregateId 
+{
+    protected AggregateId(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new Exception(); // TODO
+
+        Value = value;
+    }
+
+    private string Value { get; }
+
+    public override string ToString() => Value;
+
+    public static implicit operator string(AggregateId id) => id.Value;
+
+    public void Deconstruct(out string value) => value = Value;
+}
